721dbc1b152d69e24775a78c391d00535f5169f1
Teselapfer <schwarz44571@gmail.com>
2026-01-17 17:06:52 +0900

    update: ページのタイトルを変更

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../pages/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Teselapfer Portfolio" },
+    { name: "description", content: "Welcome to Teselapfer's portfolio!" },
   ];
 }
 
