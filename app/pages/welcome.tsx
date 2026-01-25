f999cc38037cc499ad4f7222dd49b8129a7b01f8
Teselapfer <schwarz44571@gmail.com>
2026-01-25 16:45:49 +0900

    add: Topページの概形を作成

--- app/pages/welcome.tsx
+++ app/pages/welcome.tsx
@@ -1,5 +1,8 @@
-import ContentArea from "~/components/ContentArea";
 import type { Route } from "../+types/root";
+import ContentArea from "~/components/ContentArea";
+import Section from "~/components/Section";
+import styles from "./welcome.module.css";
+import { NavLink } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +15,38 @@ export default function Welcome() {
   return (
     <div>
       <ContentArea>
-        <h1>Top Page</h1>
+        <div className={styles.head}>Welcome to teselapfer.dev!</div>
+        <div className={styles.description}>
+          初めまして。
+          <br />
+          デザインの視点を活かした創作活動を行っています。
+        </div>
+
+        <Section title="お知らせ" titleEng="NEWS">
+          <ul className={styles.newsList}>
+            <li>2026.01.25 ポートフォリオを公開しました。</li>
+          </ul>
+        </Section>
+
+        <Section title="最新の記事" titleEng="LATEST POSTS">
+          <div>
+            技術的な備忘録や日々の制作のこと、何気ない独り言を綴っています。
+          </div>
+          <NavLink to={"/blog"} className={styles.link}>
+            記事一覧を見る →
+          </NavLink>
+        </Section>
+
+        <Section title="最新の動画" titleEng="LATEST WORKS">
+          <div>制作した動画です。</div>
+          <NavLink to={"/blog"} className={styles.link}>
+            Youtubeを見る →
+          </NavLink>
+        </Section>
+
+        <Section title="激写！山口の暮らし" titleEng="PHOTOS">
+          <div>暮らしのひととき、刹那の記録。</div>
+        </Section>
       </ContentArea>
     </div>
   );
