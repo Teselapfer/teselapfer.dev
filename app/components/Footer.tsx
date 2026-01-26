a814dc525ac0d19ad1bb4d2c182826f1473b7b72
Teselapfer <schwarz44571@gmail.com>
2026-01-27 00:56:06 +0900

    add: FooterにSNSのアイコンを追加

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router";
+import github from "../assets/sns_github.svg";
+import instagram from "../assets/sns_instagram.svg";
+import twitter from "../assets/sns_twitter.svg";
+import youtube from "../assets/sns_youtube.svg";
 import styles from "./footer.module.css";
 
 const Header = () => {
@@ -8,7 +12,23 @@ const Header = () => {
         <div className={styles.copyRight}>
           ©2025 Teselapfer All rights reserved.
         </div>
-        <div className={styles.snsLink}>mail</div>
+        <div className={styles.snsLinks}>
+          <Link to="https://github.com/Teselapfer" className={styles.link}>
+            <img src={github} alt="github" />
+          </Link>
+          <Link to="" className={styles.link}>
+            <img src={instagram} alt="instagram" />
+          </Link>
+          <Link to="" className={styles.link}>
+            <img src={twitter} alt="twitter" />
+          </Link>
+          <Link
+            to="https://www.youtube.com/@teselapfer_"
+            className={styles.link}
+          >
+            <img src={youtube} alt="youtube" />
+          </Link>
+        </div>
       </div>
     </>
   );
