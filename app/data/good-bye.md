ffa2853c2fe7cbe639324f5f900ebf9a075be489
Teselapfer <schwarz44571@gmail.com>
2026-02-01 03:55:09 +0900

    update: エラーに伴う雑多な修正

---
+++ app/data/good-bye.md
@@ -0,0 +1,9 @@
+---
+title: "Good bye"
+date: "2026-01-25"
+tags: ["React", "WebDesign"]
+thumbnail: "/public/thumbnails/blog-thumb.jpg"
+---
+
+本文はここから書く。画像もいける。
+![イメージ](/public/images/roba_plane.svg)
--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./assets/roba_plane.svg" />
+        <link rel="icon" href="../app/assets/roba_plane.svg" />
         <Meta />
         <Links />
       </head>
